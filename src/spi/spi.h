4c6af43bad9b1d2695e675e1d29857d83d7648fb
Rebeca Alencar <rvsa@cesar.org.br>
2016-08-16 10:26:55 -0300

    spi: Add deinitialization function to spi in arduino
    This patch adds the deinit function for arduino spi.

--- src/spi/spi.h
+++ src/spi/spi.h
@@ -6,7 +6,20 @@
  * of the BSD license. See the LICENSE file for details.
  *
  */
+//necessary to compile in arduino
+#ifdef __cplusplus
+extern "C"{
+#endif
 
+#ifndef	ARDUINO
 int spi_init(const char *dev);
+#else
+int spi_init(void);
+#endif
+
 int spi_transfer(void *tx, uint16_t ltx, void *rx, uint16_t lrx);
 void spi_deinit(void);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
--- src/spi/spi_arduino.c
+++ src/spi/spi_arduino.c
@@ -15,6 +15,8 @@
 #define MISO	4
 #define SCK		5
 
+#define DELAY_US	5	// CSN delay in microseconds
+
 static bool	m_init = false;
 
 void spi_init(void)
@@ -47,3 +49,48 @@ void spi_init(void)
 	}
 }
 
+void spi_deinit(void)
+{
+	if(m_init) {
+		m_init = false;
+
+		PORTB |= (1 << CSN);
+
+		//Disable SPI and reset master
+		SPCR &= ~((1 << SPE) | (1 << MSTR));
+	}
+}
+
+bool spi_transfer(void *tx, uint16_t ltx, void *rx, uint16_t lrx)
+{
+	uint8_t* pd;
+
+	if(!m_init) {
+		return false;
+	}
+
+	PORTB &= ~(1 << CSN);
+	_delay_us(DELAY_US);
+
+	if(tx != NULL && ltx != 0) {
+		for(pd=(uint8_t*)tx; ltx != 0; --ltx, ++pd) {
+			SPDR = *pd;
+			asm volatile("nop");
+			while(!(SPSR & (1<<SPIF)));
+			SPDR;
+		}
+	}
+
+	if(rx != NULL && lrx != 0) {
+		for(pd=(uint8_t*)rx; lrx != 0; --lrx, ++pd) {
+			SPDR = *pd;
+			asm volatile("nop");
+			while(!(SPSR & (1<<SPIF)));
+			*pd = SPDR;
+		}
+	}
+
+	PORTB |= (1 << CSN);
+
+	return true;
+}
