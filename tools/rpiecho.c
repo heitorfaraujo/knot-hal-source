b3603069f9fde0b9ac50bbd3e49b0890ea0c22e3
Heitor Araujo <hfa@cesar.org.br>
2016-08-31 01:35:09 -0300

    tools: Add initial state machine server-client
    This patch adds an initial state machine sketch for
    server and client operation modes

--- tools/rpiecho.c
+++ tools/rpiecho.c
@@ -7,18 +7,21 @@
  *
  */
 
-/*
- * Build instructions while a Makefile is not available:
- * gcc $(pkg-config --libs --cflags glib-2.0) -Iinclude -Itools \
- * tools/rpiecho.c -o tools/rpiechod
- */
-
 #include <errno.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include <glib.h>
+#include <stdbool.h>
+
+#include "spi.h"
+#include "nrf24l01.h"
+
+enum states {
+	SEND,
+	RECEIVE
+};
 
 static char *opt_mode = "server";
 
@@ -27,16 +30,20 @@ static GMainLoop *main_loop;
 static void sig_term(int sig)
 {
 	g_main_loop_quit(main_loop);
+	nrf24l01_deinit();
+
 }
 
-static gboolean timeout_watch(gpointer user_data)
+static gboolean timeout_watch_client(gpointer user_data)
 {
+
 	/*
 	 * This function gets called frequently to verify
 	 * if there is SPI/nRF24L01 data to be read or
 	 * written. Later GPIO sys interface can be used
 	 * to avoid busy loop.
 	 */
+
 	uint8_t buffer[] = {
 		0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
 		0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
@@ -46,19 +53,64 @@ static gboolean timeout_watch(gpointer user_data)
 
 	static uint32_t counter = 0;
 	int index;
+	static enum states state = SEND;
 
-	counter++;
+	switch (state) {
 
-	buffer[28] = counter >> 24;
-	buffer[29] = counter >> 16;
-	buffer[30] = counter >> 8;
-	buffer[31] = counter;
+	case SEND: /* Send the message */
 
-	for (index = 0; index < 32; index++) {
-		if (index % 8 == 0)
+		counter++;
+
+		buffer[28] = counter >> 24;
+		buffer[29] = counter >> 16;
+		buffer[30] = counter >> 8;
+		buffer[31] = counter;
+
+		for (index = 0; index < 32; index++) {
+			if (index % 8 == 0)
 			printf("\n");
 
-		printf("0x%02x ", buffer[index]);
+			printf("0x%02x ", buffer[index]);
+		}
+
+		/* Send the Buffer here */
+
+		state = RECEIVE;
+		break;
+
+	case RECEIVE:
+
+		/* Receives the message sent here */
+		state = SEND;
+		break;
+	}
+
+	return TRUE;
+}
+
+static gboolean timeout_watch_server(gpointer user_data)
+{
+
+	static enum states state = RECEIVE;
+
+	/*
+	 * The server receive the message from the
+	 * client and send back the message received.
+	 */
+
+	switch (state) {
+
+	case SEND:
+
+		/* Send the received message here */
+		state = RECEIVE;
+		break;
+
+	case RECEIVE:
+
+		/* Receive the messagem from client here */
+		state = SEND;
+		break;
 	}
 
 	return TRUE;
@@ -74,8 +126,7 @@ int main(int argc, char *argv[])
 {
 	GOptionContext *context;
 	GError *gerr = NULL;
-	int err, timeout_id;
-
+	int timeout_id;
 	signal(SIGTERM, sig_term);
 	signal(SIGINT, sig_term);
 	signal(SIGPIPE, SIG_IGN);
@@ -94,9 +145,14 @@ int main(int argc, char *argv[])
 
 	main_loop = g_main_loop_new(NULL, FALSE);
 
-	printf("RPi nRF24L01 Radio test tool\n");
+	printf("RPi nRF24L01 Radio test tool %s mode\n", opt_mode);
 
-	timeout_id = g_timeout_add_seconds(1, timeout_watch, NULL);
+	if (strcmp(opt_mode, "client") == 0)
+		timeout_id = g_timeout_add_seconds(1,
+						timeout_watch_client, NULL);
+	else
+		timeout_id = g_timeout_add_seconds(1,
+						timeout_watch_server, NULL);
 
 	g_main_loop_run(main_loop);
 
