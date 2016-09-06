57fcb5cf2b597ca7edcf9ab15c1b80f78c7a6793
Heitor Araujo <hfa@cesar.org.br>
2016-09-06 01:58:58 -0300

    nrf24l01_client: Add a sketch of the function to open connection
    This patch adds the initial function skeleton to nrf24l01 client open,
    which will send join request to gateway.

--- src/nrf24l01/nrf24l01_client.c
+++ src/nrf24l01/nrf24l01_client.c
@@ -10,13 +10,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <stdbool.h>
 
 #include "nrf24l01-mac.h"
 #include "nrf24l01_client.h"
+#include "nrf24l01.h"
+#include "phy_driver.h"
+
+typedef struct {
+	uint8_t		pipe;
+} client_t;
+
+
+static int16_t	m_fd = SOCKET_INVALID;
+static client_t		m_client;
+static version_t	*m_pversion = NULL;
 
 int16_t nrf24l01_client_open(int16_t socket, uint8_t channel,
 							version_t *pversion)
 {
+
+	if (socket == SOCKET_INVALID)
+		return -EBADF;
+
+	if (m_fd != SOCKET_INVALID)
+		return -EMFILE;
+
+	//set channel
+	if (pversion == NULL || nrf24l01_set_channel(channel) == -1)
+		return -EINVAL;
+
+	memset(&m_client, 0, sizeof(m_client));
+	m_client.pipe = BROADCAST;
+	m_fd = socket;
+	m_pversion = pversion;
+
+	/* TO DO */
+	/* call join_local here */
 	return -1;
 }
 
