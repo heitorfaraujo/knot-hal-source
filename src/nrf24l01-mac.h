d947106d2d55fa0aecac296b5936e2db8ffb6409
Heitor Araujo <hfa@cesar.org.br>
2016-09-06 02:10:09 -0300

    nrf24l01_client: Add function to build data to be send.
    This patch adds the function that builds the data to be send.

--- src/nrf24l01-mac.h
+++ src/nrf24l01-mac.h
@@ -12,6 +12,26 @@
 
 #define BROADCAST			NRF24_PIPE0_ADDR
 
+// network retransmiting parameters
+#define NRF24_DELAY_MS		(((NRF24_ARD + 1) * \
+						NRF24_ARD_FACTOR_US) / 1000)
+
+#define NRF24_TIMEOUT_MS			(NRF24_DELAY_MS * NRF24_ARC)
+#define NRF24_HEARTBEAT_SEND_MS		2000
+#define NRF24_HEARTBEAT_TIMEOUT_MS	(NRF24_HEARTBEAT_SEND_MS * 3)
+#define NRF24_RETRIES				200
+
+// constant to SEND time values
+//	SEND_RANGE_MS_MIN <= SEND <= (SEND_RANGE_MS * SEND_FACTOR)
+#define SEND_FACTOR					1
+#define SEND_RANGE_MS_MIN	NRF24_DELAY_MS
+#define SEND_RANGE_MS				NRF24_TIMEOUT_MS
+
+// constant retry values
+#define JOINREQ_RETRY	NRF24_RETRIES
+#define SEND_RETRY		((NRF24_HEARTBEAT_TIMEOUT_MS - \
+		(NRF24_HEARTBEAT_SEND_MS + SEND_RANGE_MS)) / SEND_RANGE_MS)
+
 /**
  * struct version_t - network layer version
  * @major: protocol version, major number
