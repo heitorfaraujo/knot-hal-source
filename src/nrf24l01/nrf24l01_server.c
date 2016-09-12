7456c01cdbcad72685dcb09ca9586d04bb11d932
Heitor Fonseca <hfa@cesar.org.br>
2016-09-12 02:44:18 -0300

    nrf24l01_server: Add function that send the data.
    This patch adds the function that send data to be transmitted by
    the radio.

--- src/nrf24l01/nrf24l01_server.c
+++ src/nrf24l01/nrf24l01_server.c
@@ -32,6 +32,18 @@ typedef struct {
 				raw[];
 } data_t;
 
+typedef struct  {
+	int			fdsock,
+					pipe;
+	uint16_t	net_addr,
+					packet_size;
+	uint32_t	hashid;
+
+	uint8_t		rxmn,
+					txmn;
+	data_t		*rxmsg;
+} client_t;
+
 static data_t *build_data(int pipe, int net_addr, int msg_type,
 	void *praw, uint16_t len, data_t *msg)
 {
@@ -50,6 +62,41 @@ static data_t *build_data(int pipe, int net_addr, int msg_type,
 		memcpy(msg->raw + size, praw, len);
 	}
 	return msg;
+}
+
+/* send data to be transmitted by the radio */
+static int send_data(data_t *pdata, client_t *pc)
+{
+	payload_t payload;
+	uint8_t msg_type = pdata->msg_type;
+	int len = pdata->len;
+
+	if (msg_type == NRF24_APP && len > NRF24_PW_MSG_SIZE) {
+		msg_type = NRF24_APP_LAST;
+		if (pdata->offset == 0) {
+			msg_type = NRF24_APP_FIRST;
+			len = NRF24_PW_MSG_SIZE;
+		} else {
+			len -= pdata->offset;
+			if (len > NRF24_PW_MSG_SIZE) {
+				msg_type = NRF24_APP_FRAG;
+				len = NRF24_PW_MSG_SIZE;
+			}
+		}
+	}
+	payload.hdr.msg_xmn = MSGXMN_SET(msg_type, pc != NULL ? pc->txmn : 0);
+	payload.hdr.net_addr = pdata->net_addr;
+	memcpy(payload.msg.raw, pdata->raw + pdata->offset, len);
+	pdata->offset_retry = pdata->offset;
+	pdata->offset += len;
+
+	nrf24l01_set_ptx(pdata->pipe);
+	nrf24l01_ptx_data(&payload, len + sizeof(hdr_t), pc != NULL);
+
+	/* returns 0 if success */
+	len = nrf24l01_ptx_wait_datasent();
+	nrf24l01_set_prx();
+	return len;
 }
 
 int nrf24l01_server_open(int socket, int channel, version_t *pversion,
