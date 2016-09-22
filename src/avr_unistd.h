60bb17a6b02c88218d403e8ec45fdc7d570146ea
Heitor Fonseca <hfa@cesar.org.br>
2016-09-22 11:32:03 -0300

    avr_unistd: Add unistd header file for Arduino
    This patch adds the avr_unistd header. It defines miscellaneous symbolic
    constants and types.

---
+++ src/avr_unistd.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2015, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+#include <stdlib.h>
+
+typedef long int ssize_t;
